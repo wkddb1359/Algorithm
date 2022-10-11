03ad5c7bef48a265c8019abbcd0563896c93ac2d
SeungJu <asjj8051@gmail.com>
2022-10-11 18:01:29 +0900

    solve Factorial

--- Factorial.js
+++ Factorial.js
@@ -1,1 +1,16 @@
-// for first commit
+function factorial(n) {
+  let answer = 1;
+  for (let i = 1; i <= n; i++) {
+    answer = i * answer;
+  }
+  return answer;
+}
+
+console.log("반복문 factorial : ", factorial(5));
+
+function newFactorial(n) {
+  if (n < 1) return 1;
+  return n * newFactorial(n - 1);
+}
+
+console.log("재귀함수 factorial : ", newFactorial(5));
